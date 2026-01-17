85154a47e76aa981b211e8637fdf29784ff06906
aminumuhammad000 <aminumuhammad00015@gmail.com>
2026-01-17 17:13:39 +0100

    a

--- vtpay/vtpay-server/dist6/middleware/webhookSignature.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-/**
- * Middleware to verify Zainpay webhook signature
- */
-export declare const verifyWebhookSignature: (req: Request, res: Response, next: NextFunction) => void;
-export default verifyWebhookSignature;
-//# sourceMappingURL=webhookSignature.d.ts.map
